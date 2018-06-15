8d0e54e4084fd6c04e6f75390dd140e3e2a1490d
reyjp <reinerpena@gmail.com>
2018-06-14 23:55:22 -0430

    Commit inicial

---
+++ application/views/footer.php
@@ -0,0 +1,16 @@
+
+   <hr/>
+    <footer>
+<center>
+        <h6> Desarrollo Digital 2018 Derechos Reservados </h6>
+    
+   </center>
+    </footer>
+      	
+   
+
+
+
+
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,1 @@
+## Catalagos con Code Igniter
