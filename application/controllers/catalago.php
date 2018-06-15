92d655f194eb1f7149b81dd5203c037b6d4117f3
reyjp <reinerpena@gmail.com>
2018-06-15 18:19:54 -0430

    Funciones y clases para agregar imagen al Catalago

--- application/controllers/catalago.php
+++ application/controllers/catalago.php
@@ -72,6 +72,105 @@ class catalago extends CI_Controller
 		return true;
 		}
 	}
+
+	class FotoController extends Controller
+{
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+	public function getIndex(MostrarFotosRequest $request)
+	{
+		$id = $request->get('id');
+
+		$fotos = Album::find($id)->fotos;
+
+		return view('fotos.mostrar', ['fotos' => $fotos, 'id' => $id]);
+
+	}
+	
+	public function getCrearFoto(Request $request)
+	{
+		$id = $request->get('id');
+		return view('fotos.crear-foto', ['id' => $id]);
+	}
+
+	public function postCrearFoto(CrearFotoRequest $request)
+	{
+		$id = $request->get('id');
+		$imagen = $request->file('imagen');
+
+		$ruta = '/img/';
+		$nombre = sha1(Carbon::now()).'.'.$imagen->guessExtension();
+
+		$imagen->move(getcwd().$ruta, $nombre);
+
+		Foto::create
+		(
+			[
+				'nombre' => $request->get('nombre'),
+				'descripcion' => $request->get('descripcion'),
+				'ruta' => $ruta.$nombre,
+				'IMAGEN_id' => $id
+			]
+		);
+
+		return redirect("/validado/fotos?id=$id")->with('creada', 'La foto ha sido subida');
+	}
+
+	public function getActualizarFoto($id)
+	{
+		$foto = Foto::find($id);
+		return view('fotos.actualizar-foto', ['foto' => $foto]);
+	}
+
+	public function postActualizarFoto(ActualizarFotoRequest $request)
+	{
+		$foto = Foto::find($request->get('id'));
+
+		$foto->nombre = $request->get('nombre');
+		$foto->descripcion = $request->get('descripcion');
+
+		if($request->hasFile('IMAGEN'))
+		{
+			$imagen = $request->file('IMAGEN');
+			$ruta = '/img/';
+			$nombre = sha1(Carbon::now()).'.'.$imagen->guessExtension();
+
+			$imagen->move(getcwd().$ruta, $nombre);
+
+			$rutaanterior = getcwd().$foto->ruta;
+
+			if(file_exists($rutaanterior))
+			{
+				unlink(realpath($rutaanterior));
+			}
+
+			$foto->ruta = $ruta.$nombre;
+		}
+
+		$foto->save();
+
+		return redirect("/validado/fotos?id=$foto->IMAGEN_id")->with('editada', 'la foto fue editada');
+	}
+
+	public function postEliminarFoto(EliminarFotoRequest $request)
+	{
+		$foto = Foto::find($request->get('IMAGEN'));
+
+		$rutaanterior = getcwd().$foto->ruta;
+
+		if(file_exists($rutaanterior))
+		{
+			unlink(realpath($rutaanterior));
+		}
+
+		$foto->delete();
+
+
+		return redirect("/validado/fotos?id=$foto->album_id")->with('eliminada', 'la foto fue eliminada');
+	}
 	function select_estatus($campo)
 	{
 		// Validamos Estatus
